7c0930f2034cdd11d4efe8f74a9097663547c273
electronmachine <electronmachine57@gmail.com>
2017-05-17 09:22:27 +0530

    Learning javascritp algorithm added with Best Practices doc

---
+++ Javascript DS and Algorihm Book/BasicJavascript/BestPractices.md
@@ -0,0 +1,4 @@
+1. In Web pages, always load javascript files after body loads.
+2. always use var, when declaring new variable.
+3. Quality of javascript code is sometime measured by counting global variables. ( Because large number of global variable is very bad)
+4. learn how to use bitwise operators to make algorithm fast.
---
+++ Javascript DS and Algorihm Book/BasicJavascript/Variable Scopes/basicScope.js
@@ -0,0 +1,28 @@
+var myVariable = 'global';
+myOtherVariable = 'global';
+
+
+function myFunction(){
+ var myVariable = 'local';
+ return myVariable;
+}
+
+function myFunctionAgain(){
+	myVariable = 'local';
+	return myVariable;
+}
+
+
+function myOtherFunction(){
+ myOtherVariable = 'local';
+ return myOtherVariable;
+}
+
+console.log(myVariable); //{1}
+console.log(myFunction()); //{2}
+console.log(myVariable);
+console.log(myFunctionAgain());
+console.log(myVariable);
+console.log(myOtherVariable); //{3}
+console.log(myOtherFunction()); //{4}
+console.log(myOtherVariable);
